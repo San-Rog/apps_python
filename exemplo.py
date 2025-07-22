17c5ee730bd5cc22f8828624093ce77f5d4614de
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-07-22 18:01:40 -0300

    Update exemplo.py

--- exemplo.py
+++ exemplo.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import os
 
 def main():
     page = st.sidebar.selectbox("Escolha uma opção:", ['Opção 1', 'Opção 2', 'Opção 3'])
     st.header('Tutorial Streamlit - Heroku')
     st.write('Esta é uma página de teste.')
+    st.write(os.chdir())
 
 if __name__ == '__main__':
     main()
