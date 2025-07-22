9759161b9419fc20f90cf853b3077211b48263e1
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-07-22 18:02:48 -0300

    Update exemplo.py

--- exemplo.py
+++ exemplo.py
@@ -5,7 +5,7 @@ def main():
     page = st.sidebar.selectbox("Escolha uma opção:", ['Opção 1', 'Opção 2', 'Opção 3'])
     st.header('Tutorial Streamlit - Heroku')
     st.write('Esta é uma página de teste.')
-    st.write(os.chdir())
+    st.write(os.getcwd())
 
 if __name__ == '__main__':
     main()
